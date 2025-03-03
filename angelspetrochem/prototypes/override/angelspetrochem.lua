c3179b50d81aaad006f20c69348c6859702ec228
Gleb Golubitsky <sectoid@gnolltech.org>
2025-03-03 18:42:30 +0100

    angelspetrochem: Adaptation for Bob's changes
      * Adapted to Bob's items, fluids, techs, and recipes names
        change ('bob-' prefix)
      * Electronics no more require Angel's Coal processing tech since
        out-of-box carbon from wood recipe is provided by Bob's now

--- angelspetrochem/prototypes/override/angelspetrochem.lua
+++ angelspetrochem/prototypes/override/angelspetrochem.lua
@@ -6,26 +6,26 @@ local move_item = angelsmods.functions.move_item
 -------------------------------------------------------------------------------
 if mods["bobplates"] and data.raw["fluid"]["deuterium"] then
   angelsmods.trigger.water_green_waste = true
-  OV.converter_fluid("heavy-water", "liquid-water-heavy")
+  OV.converter_fluid("bob-heavy-water", "liquid-water-heavy")
   angelsmods.functions.disable_barreling_recipes("heavy-water")
-  OV.converter_fluid("deuterium", "gas-deuterium")
+  OV.converter_fluid("bob-deuterium", "gas-deuterium")
   angelsmods.functions.disable_barreling_recipes("deuterium")
 
   OV.disable_recipe({ "bob-heavy-water", "heavy-water-electrolysis" })
 
-  OV.global_replace_technology("heavy-water-processing", "water-chemistry-1")
-  OV.disable_technology("heavy-water-processing")
-  OV.global_replace_technology("deuterium-processing", "water-chemistry-2")
-  OV.disable_technology("deuterium-processing")
+  OV.global_replace_technology("bob-heavy-water-processing", "water-chemistry-1")
+  OV.disable_technology("bob-heavy-water-processing")
+  OV.global_replace_technology("bob-deuterium-processing", "water-chemistry-2")
+  OV.disable_technology("bob-deuterium-processing")
   OV.add_prereq("water-chemistry-2", "nuclear-fuel-reprocessing")
 
   if mods["bobrevamp"] and mods["bobpower"] and settings.startup["bobmods-revamp-nuclear"].value == true then
     -- deuterium-fuel-cell will be unlocked by bob-nuclear-power-3
   else
-    OV.add_unlock("water-chemistry-2", "deuterium-fuel-cell")
+    OV.add_unlock("water-chemistry-2", "bob-deuterium-fuel-cell")
   end
-  OV.set_science_pack("deuterium-fuel-reprocessing", "utility-science-pack", 1)
-  OV.set_science_pack("deuterium-fuel-cell-2", "utility-science-pack", 1)
+  OV.set_science_pack("bob-deuterium-fuel-reprocessing", "utility-science-pack", 1)
+  OV.set_science_pack("bob-deuterium-fuel-cell-2", "utility-science-pack", 1)
 elseif angelsmods.industries and angelsmods.industries.overhaul then
   angelsmods.trigger.water_green_waste = true
   -- everything is good, nothing to change
@@ -83,22 +83,22 @@ if angelsmods.trigger.ores["platinum"] then
     {
       name = "catalyst-metal-green",
       ingredients = {
-        { type = "item", name = "bauxite-ore", amount = 1 },
-        { type = "item", name = "silver-ore", amount = 1 },
+        { type = "item", name = "bob-bauxite-ore", amount = 1 },
+        { type = "item", name = "bob-silver-ore", amount = 1 },
       },
     },
     {
       name = "catalyst-metal-blue",
       ingredients = {
-        { type = "item", name = "rutile-ore", amount = 1 },
-        { type = "item", name = "gold-ore", amount = 1 },
+        { type = "item", name = "bob-rutile-ore", amount = 1 },
+        { type = "item", name = "bob-gold-ore", amount = 1 },
       },
     },
     {
       name = "catalyst-metal-yellow",
       ingredients = {
-        { type = "item", name = "tungsten-ore", amount = 1 },
-        { type = "item", name = "platinum-ore", amount = 1 },
+        { type = "item", name = "bob-tungsten-ore", amount = 1 },
+        { type = "item", name = "bob-platinum-ore", amount = 1 },
       },
     },
   })
@@ -115,22 +115,22 @@ elseif mods["bobplates"] or angelsmods.industries and angelsmods.industries.over
     {
       name = "catalyst-metal-green",
       ingredients = {
-        { type = "item", name = "bauxite-ore", amount = 1 },
-        { type = "item", name = "silver-ore", amount = 1 },
+        { type = "item", name = "bob-bauxite-ore", amount = 1 },
+        { type = "item", name = "bob-silver-ore", amount = 1 },
       },
     },
     {
       name = "catalyst-metal-blue",
       ingredients = {
-        { type = "item", name = "rutile-ore", amount = 1 },
-        { type = "item", name = "cobalt-ore", amount = 1 },
+        { type = "item", name = "bob-rutile-ore", amount = 1 },
+        { type = "item", name = "bob-cobalt-ore", amount = 1 },
       },
     },
     {
       name = "catalyst-metal-yellow",
       ingredients = {
-        { type = "item", name = "tungsten-ore", amount = 1 },
-        { type = "item", name = "nickel-ore", amount = 1 },
+        { type = "item", name = "bob-tungsten-ore", amount = 1 },
+        { type = "item", name = "bob-nickel-ore", amount = 1 },
       },
     },
   })
